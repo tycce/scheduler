d00a223b8556fa55c3c6135c8da9f63a74dd7293
tycce Notebook <4ap-c@mail.ru>
2021-11-24 23:58:40 +0300

    init

---
+++ src/main/java/com/example/schedulertest/SchedulerTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.schedulertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulerTestApplication.class, args);
+    }
+
+}
